92108629cadad464b06ea0474e51805d8298c2c1
noui61 <noui@metavault.org>
2022-07-28 16:04:47 +0300

    createItem section update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -80,7 +80,9 @@ import { actionType } from '../context/reducer';
                             {
                                 user && user.email === "aktasmuratt7@gmail.com" && (
                                     <Link to={'/createItem'}>
-                                        <p className='px-4 py-2 flex item-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base'>New Item <MdAdd className='mt-1'/></p>
+                                        <p className='px-4 py-2 flex item-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base'
+                                        onClick={()=>setIsMenu(false)}
+                                        >New Item <MdAdd className='mt-1'/></p>
                                     </Link>
     
                                 )
@@ -133,10 +135,10 @@ import { actionType } from '../context/reducer';
                                 )
                             }
                              <ul className='flex flex-col'>
-                                <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'>Home</li>
-                                <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'>Menü</li>
-                                <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'>About us</li>
-                                <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'>Service </li>
+                                <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2' onClick={()=>setIsMenu(false)}>Home</li>
+                                <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2' onClick={()=>setIsMenu(false)}>Menü</li>
+                                <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2' onClick={()=>setIsMenu(false)}>About us</li>
+                                <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2' onClick={()=>setIsMenu(false)}>Service </li>
                              </ul>
 
                             <p className='m-2 p-2 rounded-md shadow-md flex item-center justify-center bg-gray-200 gap-3 cursor-pointer hover:bg-gray-300 transition-all duration-100 ease-in-out text-textColor text-base'
