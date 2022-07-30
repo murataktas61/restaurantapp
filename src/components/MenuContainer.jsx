40b0d3c190ed906aa873862c0ec374545a4c22c6
noui61 <noui@metavault.org>
2022-07-31 01:51:01 +0300

    home page 'Our Fresh - Healthy Fruits'  add

---
+++ src/components/MenuContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MenuContainer = () => {
+  return (
+    <section className='w-full my-6'>
+        menu section
+    </section>
+  )
+}
+
+export default MenuContainer
