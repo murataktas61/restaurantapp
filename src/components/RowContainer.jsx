39c01d7284f6bbc90fd8c32b87f2decccdecd803
noui61 <noui@metavault.org>
2022-07-31 23:42:46 +0300

    menu section add

--- src/components/RowContainer.jsx
+++ src/components/RowContainer.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useRef } from 'react'
 import { MdShoppingBasket } from 'react-icons/md'
 import { motion } from 'framer-motion'
+import NotFound from '../img/NotFound.svg'
+
 const RowContainer = ({ flag,data,scrollValue }) => {
 
   const rowContainer=useRef();
@@ -17,8 +19,8 @@ const RowContainer = ({ flag,data,scrollValue }) => {
         flag ? "overflow-x-scroll scrollbar-none" : "overflow-x-hidden flex-wrap justify-center"
       }`}
       >
-        {data && data.map((item)=>(
-
+        {data && data.length > 0 ? (
+          data.map((item)=>(
             <div key={item?.id}  className="w-275 h-[235px] min-w-[275px] md:w-300 md:min-w-[310px]  bg-cardOverlay rounded-lg py-2 px-4  my-12 backdrop-blur-lg hover:drop-shadow-lg flex flex-col items-center justify-evenly relative">
                <div className="w-full flex items-center justify-between">
               <motion.div
@@ -53,8 +55,13 @@ const RowContainer = ({ flag,data,scrollValue }) => {
               </div>
             </div>
 
-        ))}
-
+        ))
+        ) : (
+        <div className='w-full flex flex-col items-center justify-center'>
+          <img src={NotFound} className="h-340" />
+          <p className='text-xl text-headingColor font-semibold my-2'>Items Not Available</p>
+        </div>
+        )}
     </div>
   )
 }
